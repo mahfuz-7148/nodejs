0753f9597ae8d2d4d633b5f48fd557a30dd9742f
mahfuz-7148 <mrahman7148@gmail.com>
2025-10-05 01:49:24 +0600

    js updated

--- index.js
+++ index.js
@@ -61,7 +61,17 @@
 
 import http from 'http'
 const server = http.createServer((req, res) => {
-  res.write('hello devs')
+  if (req.url === '/') {
+    res.write('hello devs')
+    res.write(' hello developers')
+    res.end()
+  } else if (req.url === '/about') {
+    res.write('hello devs its about page')
+    res.end()
+  } else {
+    res.write('not found')
+    res.end()
+  }
 })
 server.listen(3000)
 console.log('listening on port 3000')
